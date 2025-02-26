940406e5346e3e77057ee10080d95e83424afae9
jeffrasnic <jcr126@humboldt.edu>
2025-02-25 17:39:05 -0800

    Basic tree working

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@ type Link<T> = NonNull<TreeNode<T>>;
 
 pub struct Tree<T> {
     head:   Link<T>,
-    depth:  u8,
-    length: u8,
     _ghost: PhantomData<T>,
 }
 
@@ -14,58 +12,57 @@ impl<T: std::cmp::PartialEq> Tree<T> {
     pub fn new() -> Self {
         Self {
             head:   NonNull::dangling(),
-            depth:  0,
-            length: 0,
             _ghost: PhantomData,
         }
     }
 
-    pub fn search_lvr(&mut self, val: T) -> Box<TreeNode<T>> {
+    pub fn set_head(&mut self, node: &mut TreeNode<T>) {
+        self.head = Link::new(node as *mut _).expect("ptr is null");
+    }
+
+    pub fn search_lvr(&mut self, _val: T) -> Box<TreeNode<T>> {
         panic!("Unimplmented!");
     }
 
     pub fn search_vlr(&mut self, val: T) -> Option<Box<TreeNode<T>>> {
+        if self.head == NonNull::dangling() {
+            return None;
+        }
+
         unsafe {
-            if self.head.read().elem == val {
-                return Some(Box::new(self.head.read()));
-            }
 
             let mut stack: Vec<Link<T>> = vec![];
-            let mut cur_node: Link<T> = self.head.read().left;
 
-            stack.push(cur_node);
+            stack.push(self.head);
 
-            /*
-             *
-             *      what's the algorithm
-             *
-             *      first we check if our only node on the stack is the one
-             *          if not, we push its left node and process that one
-             *          if we encounter a dangling node, we peek the stack
-             *          check if it has a right node and process that one
-             *          if it dangles again, then we pop because we're done with that node
-             *          if the pop is none, then we are done
-             *
-             */
+            let mut cur_node: Link<T> = stack[0];
 
-            while cur_node.read().elem != val {
-                if cur_node == NonNull::dangling() {
-                    return None;
+            while !stack.is_empty() {
+                if cur_node.read().elem == val {
+                    break;
+                } else {
+                    if cur_node.read().right != NonNull::dangling() {
+                        stack.push(cur_node.read().right);
+                    }
+
+                    if cur_node.read().left != NonNull::dangling() {
+                        stack.push(cur_node.read().left);
+                    }   
                 }
-                
-                
 
+                cur_node = stack.pop().unwrap();
             }
 
             Some(Box::new(cur_node.read()))
         }
     }
 
-    pub fn search_vrl(&mut self, val: T) -> Box<TreeNode<T>> {
+    pub fn search_vrl(&mut self, _val: T) -> Box<TreeNode<T>> {
         panic!("Unimplmented!");
     }
 }
 
+#[derive(PartialEq, Debug)]
 pub struct TreeNode<T> {
     elem:  T,
     left:  Link<T>,
@@ -73,11 +70,17 @@ pub struct TreeNode<T> {
 }
 
 impl<T> TreeNode<T> {
-    
-}
+    pub fn new(val: T) -> Self {
+        Self { elem: val, left: NonNull::dangling(), right: NonNull::dangling() }
+    }
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+    pub fn set_left(&mut self, node: &mut TreeNode<T>) {
+        self.left = Link::new(node as *mut _).expect("ptr is null");
+    }
+
+    pub fn set_right(&mut self, node: &mut TreeNode<T>) {
+        self.right = Link::new(node as *mut _).expect("ptr is null");
+    }
 }
 
 #[cfg(test)]
@@ -85,8 +88,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn tree_tests() {
+        let mut tree = Tree::new();
+        let mut head = TreeNode::new(5);
+        tree.set_head(&mut head);
+        let mut head_l = TreeNode::new(2);
+        let mut head_r = TreeNode::new(7);
+        let mut head_r_r = TreeNode::new(70);
+        head_r.set_right(&mut head_r_r);
+        head.set_left(&mut head_l);
+        head.set_right(&mut head_r);
+        let mut head_l_l = TreeNode::new(1);
+        head_l.set_left(&mut head_l_l);
+
+        let box_r = Box::new(head_r);
+        let box_l = Box::new(head_l);
+        let box_r_r = Box::new(head_r_r);
+        assert_eq!(box_r == tree.search_vlr(7).unwrap(), true);
+        assert_eq!(box_l == tree.search_vlr(2).unwrap(), true);
+        assert_eq!(box_r_r== tree.search_vlr(70).unwrap(), true);
+        assert_eq!(box_r == tree.search_vlr(1).unwrap(), false);
+        assert_eq!(box_l == tree.search_vlr(1).unwrap(), false);
+        assert_eq!(box_r_r== tree.search_vlr(10).unwrap(), false);
     }
 }
